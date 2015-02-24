b555466e3017166196e15ca6a88b129faf569fc9
Alexander Blythe <Alexander.blythe@gmail.com>
2015-02-23 23:22:52 -0500

    Added login page

--- todo_web.rb
+++ todo_web.rb
@@ -4,11 +4,32 @@ require './db/setup'
 require './lib/all'
 
 class TodoWeb < Sinatra::Base
+  enable :sessions
 
   def current_user
-    username = "ablythe"
+    username = session[:username]
     # request.env["HTTP_AUTHORIZATION"]
     User.find_by_login username
+  end
+
+  def login_checker
+    if session[:username]
+      return
+    else
+      session[:page_saver] = request.env["PATH_INFO"]
+      redirect to('/login')
+    end
+  end
+
+
+  get "/login" do
+    erb :login
+  end
+
+  post "/login" do
+    session[:username] = params[:username]
+    binding.pry
+    redirect to(session[:page_saver])
   end
 
   post "/add" do 
@@ -16,28 +37,32 @@ class TodoWeb < Sinatra::Base
     redirect back    
   end
 
-  patch "/lists/:list/tasks/:id" do 
+  post "/due" do 
     task = Task.find(params['id'])
-    if params['action'] == 'due'
-      task.add_due_date params['due_date']
-    else params['action'] == 'done'
-      task.mark_complete! 
+    task.add_due_date params['due_date']
+    redirect back
     end
-    'OK'
+
+  post "/complete" do
+    task = Task.find(params['id'])
+    task.mark_complete! 
+    redirect back
   end
 
   get "/lists/:list/tasks" do
+    login_checker
     @tasks = current_user.list_tasks params['list']
     erb :task_list
-    
   end
 
   get "/lists/all/tasks/next" do 
+    login_checker
     tasks = current_user.tasks.random
     tasks.to_json
   end
 
   get "/search" do 
+    login_checker
     search_string = params['search']
     tasks = current_user.tasks.where("description like '%#{search_string}%'")
     tasks.to_json
